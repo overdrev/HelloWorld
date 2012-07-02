99db6bab6523d3fe53939d03762046324bd3f694
Herison <herison@spotnik.net>
2012-07-02 11:07:49 +0200

    OK

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -5,8 +5,37 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
 }
+/*
+ * 
+ * 
+ * Global setup:
+ Set up git
+  git config --global user.name "Your Name"
+  git config --global user.email github@overdrev.com
+      
+Next steps:
+  mkdir HelloWorld
+  cd HelloWorld
+  git init
+  touch README
+  git add README
+  git commit -m 'first commit'
+  git remote add origin https://github.com/overdrev/HelloWorld.git
+  git push -u origin master
+      
+Existing Git Repo?
+  cd existing_git_repo
+  git remote add origin https://github.com/overdrev/HelloWorld.git
+  git push -u origin master
+      
+Importing a Subversion Repo?
+  Check out the guide for step by step instructions.
+      
+When you're done:
+  Continue
+ * 
+ * */
